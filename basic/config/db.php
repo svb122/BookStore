e19a7511ed8782330bf66145e6c3cefad07a8d71
svb122 <sbakharau@gmail.com>
2016-06-19 12:46:10 +0300

    19.06.2016 12:45

--- basic/config/db.php
+++ basic/config/db.php
@@ -8,6 +8,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=appdb',
     'username' => 'root',
-    'password' => '521026Svb',
+    'password' => 'your password',
     'charset' => 'utf8',
 ];
--- basic/config/web.php
+++ basic/config/web.php
@@ -29,7 +29,7 @@ $config = [
         ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '521026Svb',
+            'cookieValidationKey' => 'BookStoresk',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
